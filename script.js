afd258f83038f2aabf7886b2fd2de0dd49fbfd8f
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-02-15 14:47:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ $.get("https://raw.githubusercontent.com/dduyg/alchemyofsouls-quotes/main/data/a
 
     // Function to display a random quote
     function showRandomQuote() {
-      // Select a random quote from the array
-      const randomIndex = Math.floor(Math.random() * quotes.length);
-      const randomQuote = quotes[randomIndex];
+        const randomIndex = getRandomIndex(quotes.length);
+        const randomQuote = quotes[randomIndex];
 
-      // Update the HTML elements with the quote data
-      quoteText.textContent = randomQuote.text;
-      quoteCharacter.textContent = randomQuote.character;
-      quoteSeason.textContent = randomQuote.season;
-      quoteEpisode.textContent = randomQuote.episode;
+        // Check if the random quote has any missing data
+        if (!randomQuote.text || !randomQuote.character || !randomQuote.season || !randomQuote.episode) {
+            // If the quote is missing data, show an error message and do not update the HTML elements
+            alert("Error: The selected quote is missing data. Please try again.");
+            return;
+        }
+
+        // Update the HTML elements with the quote data
+        quoteText.textContent = randomQuote.text;
+        quoteCharacter.textContent = randomQuote.character;
+        quoteSeason.textContent = randomQuote.season;
+        quoteEpisode.textContent = randomQuote.episode;
+    }
+
+    // Function to generate a random index between 0 and maxIndex (exclusive)
+    function getRandomIndex(maxIndex) {
+        return Math.floor(Math.random() * maxIndex);
     }
 
     // Show a random quote when the page loads
